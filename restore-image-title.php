1f31b620af4f1fdfcc4ed490b44d4f245e3dffd1
LesBessant <LesBessant@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-12 19:47:34 +0000

    Tagging v1.5
    git-svn-id: https://plugins.svn.wordpress.org/restore-image-title/tags/1.5@964751 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- restore-image-title.php
+++ restore-image-title.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Restore Image Title
 Description: Reverses WP 3.5's behaviour of stripping title from images inserted into posts
-Version: 1.1
+Version: 1.5
 License: GPL
 Author: Les Bessant
 Author URI: http://losingit.me.uk/
@@ -20,4 +20,11 @@ function lcb_restore_image_title( $html, $id ) {
 }
 }
 add_filter( 'media_send_to_editor', 'lcb_restore_image_title', 15, 2 );
+
+function lcb_restore_title_to_gallery( $content, $id ) {
+	$thumb_title = get_the_title($id);
+	return str_replace('<a', '<a title="' . esc_attr($thumb_title) . '" ', $content);
+}	
+add_filter('wp_get_attachment_link', 'lcb_restore_title_to_gallery', 10, 4);
+
 ?>
